62131cc2ae9d04cd403d57e5aa79b38455a6b157
Jiaqi Zhu <i@zjiaqi.com>
2021-12-17 04:20:07 -0600

    update

--- PrefixSum/Leetcode370.java
+++ PrefixSum/Leetcode370.java
@@ -1,5 +1,7 @@
 class Leetcod370{
     public int[] getModifiedArray(int length,int[][] updates){
+        // in this question, we try to mark the start index,and end index, we plus first to indicate a update starting position, and minus it to mark as end position.
+        // in the end we add them together to get the final result;
         int[] prefixSum = new int[length];
         for(int[] update:updates){
             prefixSum[update[0]] += update[2];
