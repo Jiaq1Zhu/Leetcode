1811c91b03e60213d063091281408a8f14486124
Dh0le <i@zjiaqi.com>
2022-03-03 18:07:17 -0600

    Create knapsackProblems solution

---
+++ DynamicProgramming/knapsackProblems.java
@@ -0,0 +1,33 @@
+public class knapsackProblems{
+    public static int zeroOneKnapsackNonoptimized(int[] weights,int[] values, int max,int num){
+        int[][] dp = new int[num+1][max+1];
+        for(int i = 1;i <= num;i++){
+            for(int j = 1;j <= max;j++){
+                if(weights[i-1] > j){
+                    dp[i][j] = dp[i-1][j];
+                }else{
+                    dp[i][j] = Math.max(dp[i-1][j],dp[i-1][j-weights[i-1]]+values[i-1]);
+                }
+            }
+        }
+        return dp[num][max];
+    }
+
+    public static int zeroOneKnapsackOptimized(int[] weights, int[] values, int max){
+        int[] dp = new int[max+1];
+        for(int i = 1;i < weights.length+1;i++){
+            for(int j = max;j >= weights[i-1];j--){
+                dp[j] = Math.max(dp[j],dp[j-weights[i-1]]+values[i-1]);
+            }
+        }
+        return dp[max];
+    }
+
+    public static void main(String[] args) {
+        int[] values = {60,100,120};
+        int[] weight = {10,20,30};
+        int w = 50;
+        System.out.println(zeroOneKnapsackNonoptimized(weight,values,w,weight.length));
+        System.out.println(zeroOneKnapsackOptimized(weight,values,w));
+    }
+}
