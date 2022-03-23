cc2b74d05e5d32a7139f2a9e6c3abab70888df91
Dh0le <i@zjiaqi.com>
2022-03-22 22:16:24 -0500

    Refactor files, add 886 to biparted summary

--- Graph/GraphBipartedSummary.java
+++ Graph/GraphBipartedSummary.java
@@ -1,5 +1,6 @@
-class Leetcode785 {
+class GraphBipartedSummary {
 
+    // Leetcode 785
     /*
     Check if the graph can be divide into two seperate graph
     Use color method to check if two node that connect with one edge has same color, if so it is not a bipartie graph.
@@ -73,4 +74,36 @@ class Leetcode785 {
          }
          return true;
      }
+
+     // Leetcode 886
+     // It is same solution as 785, instead we need to kinda build the graph by ourselves.
+     public boolean possibleBipartition(int n, int[][] dislikes) {
+        int[] colored = new int[n+1];
+        if(dislikes == null || dislikes.length == 0)return true;
+        Map<Integer,List<Integer>> graph = new HashMap<>();
+        for(int i = 1;i <= n;i++){
+            graph.putIfAbsent(i, new ArrayList<>());
+        }
+        for(int[] dislike: dislikes){
+            graph.get(dislike[0]).add(dislike[1]);
+            graph.get(dislike[1]).add(dislike[0]);
+        }
+        for(int i = 1;i <= n;i++){
+            if(colored[i] != 0)continue;
+            colored[i] = 1;
+            Queue<Integer> q = new LinkedList<>();
+            q.offer(i);
+            while(!q.isEmpty()){
+                int curr = q.poll();
+                for(int next:graph.get(curr)){
+                    if(colored[curr] == colored[next])return false;
+                    if(colored[next] == 0){
+                        colored[next] = -1 * colored[curr];
+                        q.offer(next);
+                    }
+                }
+            }
+        }
+        return true;
+    }
 }
